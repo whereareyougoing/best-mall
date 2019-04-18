9a9caec379ab8746ebb5b630e9ae2062c5e277da
unknown <zhangzhongwen@360buyad.local>
2019-04-18 17:28:04 +0800

    product service

--- src/main/java/com/imooc/mall/service/IFileService.java
+++ src/main/java/com/imooc/mall/service/IFileService.java
@@ -1,9 +1,12 @@
 package com.imooc.mall.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  * @author 宋艾衡
  * @date 2019/4/12 15:35
  * @desc
  */
 public interface IFileService {
+    String upload(MultipartFile file, String path);
 }
